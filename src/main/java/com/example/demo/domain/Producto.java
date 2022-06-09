05cbd817840e399ab0368f361fd753e9fee0afee
Marvin <aguirreestuardo758@gmail.com>
2022-06-08 21:48:15 -0600

    Se realizaron varias implementaciones

--- src/main/java/com/example/demo/domain/Producto.java
+++ src/main/java/com/example/demo/domain/Producto.java
@@ -1,5 +1,71 @@
 package com.example.demo.domain;
 
-public class Producto {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class Producto {
+	@Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+	private Long codProduct;
+    private String productName;
+    private Long cantproduct;
+    private int precio;
+    
+    
+    public Producto() {
+
+	}
+
+	public Producto(Long id, Long codProduct, String productName, Long cantproduct, int precio) {
+		super();
+		this.id = id;
+		this.codProduct = codProduct;
+		this.productName = productName;
+		this.cantproduct = cantproduct;
+		this.precio = precio;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getCodProduct() {
+		return codProduct;
+	}
+
+	public void setCodProduct(Long codProduct) {
+		this.codProduct = codProduct;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public Long getCantproduct() {
+		return cantproduct;
+	}
+
+	public void setCantproduct(Long cantproduct) {
+		this.cantproduct = cantproduct;
+	}
+
+	public int getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(int precio) {
+		this.precio = precio;
+	}
 }
---
+++ src/main/java/com/example/demo/service/productoService.java
@@ -0,0 +1,31 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.domain.Producto;
+import com.example.demo.Repository.ProductoRepository;
+
+@Service
+public class productoService {
+	@Autowired
+    private ProductoRepository repo;
+	
+	public List<Producto> listAll() {
+        return repo.findAll();
+    }
+     
+    public void save(Producto std) {
+        repo.save(std);
+    }
+     
+    public Producto get(long id) {
+        return repo.findById(id).get();
+    }
+     
+    public void delete(long id) {
+        repo.deleteById(id);
+    }
+}
